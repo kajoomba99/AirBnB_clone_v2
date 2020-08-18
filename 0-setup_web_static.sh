fc41fb2e1503a6e72ffed50e4ae073205933c830
Steven Brand <steven.brand1999@gmail.com>
2020-08-17 23:11:49 -0500

    changes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
-sudo apt update
-sudo apt install -y nginx
+
+sudo apt-get update
+sudo apt-get install -y nginx
 ufw allow 'Nginx HTTP'
 mkdir -p "/data/web_static/releases/test/"
 mkdir -p "/data/web_static/shared/"
-touch "/data/web_static/releases/test/index.html"
 cat >> "/data/web_static/releases/test/index.html" <<EOL
 <html>
   <head>
@@ -15,7 +15,7 @@ cat >> "/data/web_static/releases/test/index.html" <<EOL
   </body>
 </html>
 EOL
-ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
 sed -i '/server_name _;/ a\location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-available/default
 /etc/init.d/nginx restart
