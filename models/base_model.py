f455893744f52242632ef018cef41d975e0382aa
Steven Brand <steven.brand1999@gmail.com>
2020-09-01 17:28:05 -0500

    Updates

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,8 @@ import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
-import os
+import models
+
 
 Base = declarative_base()
 
@@ -18,42 +19,34 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+
         if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-
+            if kwargs.get("created_at"):
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+            if kwargs.get("updated_at"):
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
             if "id" not in kwargs:
                 kwargs["id"] = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                kwargs["created_at"] = datetime.now()
-            else:
-                kwargs['created_at'] = datetime.strptime(
-                    kwargs['created_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f'
-                )
-            if "updated_at" not in kwargs:
-                kwargs["updated_at"] = datetime.now()
-            else:
-                kwargs['updated_at'] = datetime.strptime(
-                    kwargs['updated_at'],
-                    '%Y-%m-%dT%H:%M:%S.%f'
-                )
 
-            if "__class__" in kwargs:
+            elif "__class__" in kwargs:
                 del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        dictionary = self.__dict__
-        if "__class__" in dictionary:
-            del dictionary['__class__']
-        if '_sa_instance_state' in dictionary:
-            dictionary.pop("_sa_instance_state")
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, dictionary)
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
@@ -67,14 +60,13 @@ class BaseModel:
         dictionary = {}
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if '_sa_instance_state' in dictionary:
-            del dictionary["_sa_instance_state"]
+        dictionary.pop('_sa_instance_state', None)
         return dictionary
 
     def delete(self):
-        """delete the current instance from the storage models.storage"""
-        from models import storage
-        storage.delete(self)
+        """Delete current instance of BaseModel class."""""
+
+        models.storage.delete(self)
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""starts a Flask web application"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is(text):
+    return "C {}".format(text.replace("_", " "))
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is(text):
+    return "Python {}".format(text.replace("_", " "))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def integer(n:int):
+    return "{} is a number".format(n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
