c6fa8ce12b05ade72e54a4647399eb935881d397
Steven Brand <steven.brand1999@gmail.com>
2020-09-01 17:44:19 -0500

    updates

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,13 +3,16 @@
 from flask import Flask, escape
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is(text):
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -3,17 +3,21 @@
 from flask import Flask, escape
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is(text):
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
