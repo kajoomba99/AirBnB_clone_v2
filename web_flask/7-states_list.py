8cd07e8d6052853e29c19806ef0999ec0d2decbd
Steven Brand <steven.brand1999@gmail.com>
2020-09-02 14:32:15 -0500

    Teash #8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,40 +2,20 @@
 """starts a Flask web application"""
 from flask import Flask, escape, render_template
 from models import storage
+from models.state import State
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is(text):
-    return "C {}".format(text.replace("_", " "))
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_is(text):
-    return "Python {}".format(text.replace("_", " "))
+@app.teardown_appcontext
+def tear_dow(void):
+    storage.close()
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def integer(n:int):
-    return "{} is a number".format(n)
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n:int):
-    return render_template('5-number.html', n=n)
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n:int):
-    even_or_odd = "even"
-    if n % 2 != 0:
-        even_or_odd = "odd"
-
-    return render_template('6-number_odd_or_even.html', n= n, odd_even= even_or_odd)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
